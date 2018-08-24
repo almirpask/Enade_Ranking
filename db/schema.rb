fcc37c703c4a5aaba3290ae7faa18adcb525e469
Almir <almirpask@hotmail.com>
2018-08-23 21:54:28 -0300

    Create courses resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_23_213654) do
+ActiveRecord::Schema.define(version: 2018_08_24_003119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_courses_on_name", unique: true
+  end
 
   create_table "institutions", force: :cascade do |t|
     t.string "name"
@@ -23,4 +30,17 @@ ActiveRecord::Schema.define(version: 2018_08_23_213654) do
     t.index ["name"], name: "index_institutions_on_name", unique: true
   end
 
+  create_table "rankings", force: :cascade do |t|
+    t.bigint "course_id"
+    t.bigint "institution_id"
+    t.decimal "course_score", precision: 5, scale: 2, default: "0.0"
+    t.decimal "student_score", precision: 5, scale: 2, default: "0.0"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_rankings_on_course_id"
+    t.index ["institution_id"], name: "index_rankings_on_institution_id"
+  end
+
+  add_foreign_key "rankings", "courses"
+  add_foreign_key "rankings", "institutions"
 end
